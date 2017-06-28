e618713c2dcf7665ed81f5eea186b976b0ea147e
Mikeytjuh <0906393@hr.nl>
2017-06-28 12:21:36 +0200

    Dataset altered and added funtion to parse location

--- app/src/main/java/com/roadtrippies/app/roadtrippies/NearbyActivity.java
+++ app/src/main/java/com/roadtrippies/app/roadtrippies/NearbyActivity.java
@@ -1,6 +1,10 @@
 package com.roadtrippies.app.roadtrippies;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +14,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 
 public class NearbyActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -30,6 +40,34 @@ public class NearbyActivity extends AppCompatActivity implements OnMapReadyCallb
         }
 
         gMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-        gMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        gMap.addMarker(new MarkerOptions().position(getLocationFromAddress(this, "Raampoortstraat 24/26/28Rotterdam NL\n")).title("Event 1"));
     }
+
+    public LatLng getLocationFromAddress(Context context, String strAddress)
+    {
+        Geocoder coder= new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try
+        {
+            address = coder.getFromLocationName(strAddress, 5);
+            if(address==null)
+            {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return p1;
+
+    }
+
 }
